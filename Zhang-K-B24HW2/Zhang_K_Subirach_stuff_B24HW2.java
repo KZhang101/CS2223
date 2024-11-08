68292d9fa78972125c8e644ee5925502afef5d3b
Kang Zhang <originalsuperarmd@gmail.com>
2024-11-08 09:39:16 -0500

    Added comments and what to run

--- Zhang-K-B24HW2/Zhang_K_Subirach_stuff_B24HW2.java
+++ Zhang-K-B24HW2/Zhang_K_Subirach_stuff_B24HW2.java
@@ -3,16 +3,24 @@ public class Zhang_K_Subirach_stuff_B24HW2 {
     public static class subirachs{
         private int[] magicList = {1, 14, 14, 4, 11, 7, 6, 9, 8, 10, 10, 5, 13, 2, 3, 15};
         private int magicSize = magicList.length;
-        private int checker = 0;
+        private int checker = 0; // Debugging tool 
 
         public subirachs(){}
 
+        /**
+         * Checks if a list equals to the magic number
+         */
         private boolean isSum(int[] nums, int magicNumber){
             int sum = 0;
             for(int i=0; i < nums.length; i++) sum+=nums[i];
             return sum==magicNumber;
         } 
 
+        /**
+         * Finds the number of subsets of size 4 that sum to the magic number 
+         * Run this function
+         * @return
+         */
         private int all4Subsets(){
             int count = 0;
             int combination = 0; // C(16, 4) = 1820
@@ -22,8 +30,7 @@ public class Zhang_K_Subirach_stuff_B24HW2 {
                     for (int b3 = b2 + 1; b3 < this.magicSize - 1; b3++) {
                         for (int b4 = b3 + 1; b4 < this.magicSize; b4++) {
                             int[] subset = new int[]{magicList[b1], magicList[b2], magicList[b3], magicList[b4]};
-                            //printList(new int[]{b1, b2, b3, b4});
-                            //this.base[combination] = new int[]{b1, b2, b3, b4};
+                            //printList(new int[]{b1, b2, b3, b4}); // Debugging right combinations 
                             count += isSum(subset, 33) ? 1 : 0;
                             combination++;
                         }
@@ -36,6 +43,16 @@ public class Zhang_K_Subirach_stuff_B24HW2 {
             return count;
         }
 
+        /**
+         * Recursive algorithm to find all possible combinations 
+         * @param start Starting index 
+         * @param depth Current index position in generating combination 
+         * @param indices Current indices stored to find sum 
+         * @param count Current count of sum 33 
+         * @param size Size of the subset
+         * @param magicNumber Magic number sum to be found 
+         * @return number of combinations that sum to the magic number 
+         */
         private int countSubsets(int start, int depth, int[] indices, int count, int size, int magicNumber) {
             if (depth == size) { 
                 int[] subset = new int[size];
@@ -53,6 +70,10 @@ public class Zhang_K_Subirach_stuff_B24HW2 {
             return count;
         }
 
+
+        /**
+         * Finds all possible subsets from 0 - 16 to find the magic sum 33 
+         */
         private void allXSubsets(){
             for(int size = 0; size <= this.magicSize; size++){
                 this.checker = 0;
@@ -63,12 +84,16 @@ public class Zhang_K_Subirach_stuff_B24HW2 {
             }
         }
 
+        /**
+         * Finds all combinations sizes for all possible sums.
+         * Call this function
+         */
         private void allPossibleSums(){
             int max = 0;
-            for(int i = 0; i<this.magicSize; i++) max += this.magicList[i];
-            for(int sumCheck = 0; sumCheck<=max; sumCheck++){
+            for(int i = 0; i<this.magicSize; i++) max += this.magicList[i]; // Find sum of all 16 numbers 
+            for(int sumCheck = 0; sumCheck<=max; sumCheck++){ // Iterate throug all sums between 0-max
                 System.out.println("Checking Sum: " + sumCheck);
-                for(int size = 0; size <= this.magicSize; size++){
+                for(int size = 0; size <= this.magicSize; size++){ // find all combinations for x size and check for sum 
                     this.checker = 0;
                     int count = countSubsets(0, 0, new int[size], 0, size, sumCheck);
                     System.out.print("Subset Size: " + size); 
